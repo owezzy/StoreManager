bfade4e99606457b73c21ae2f67c5a06d5b3fb34
owen_adira <owenobezzy@gmail.com>
2018-10-16 22:37:28 +0300

    [Feature #161129703] update js file

---
+++ js/main.js
@@ -0,0 +1,10 @@
+//side menu functionality
+function openNav() {
+    document.getElementById("side-menu").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("side-menu").style.width = "0";
+}
+
+//filter using store attendants name
