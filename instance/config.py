d6d248116082a1f61e6c3bdb965813b401309eb8
owen_adira <owenobezzy@gmail.com>
2018-10-18 19:26:07 +0300

    update app instance configuration

---
+++ instance/config.py
@@ -0,0 +1,42 @@
+"""
+This file sets the configurations for the application
+"""
+import os
+
+
+class Config(object):
+    """Parent configuration class."""
+    DEBUG = False
+    CSRF_ENABLED = True
+    SECRET_KEY = os.getenv("SECRET_KEY", "owezzy")
+    DATABASE_URL = os.getenv("DATABASE_URL")
+
+
+"""
+    "dbname='owezzystore' host='localhost' port='5432' user='owen' password='abc@123'"
+"""
+
+
+class DevelopmentConfig(Config):
+    """Development phase configurations"""
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    """Testing Configurations."""
+    TESTING = True
+    DEBUG = True
+    DATABASE_URL = os.getenv("DATABASE_TEST_URL")
+
+
+class ReleaseConfig(Config):
+    """Release Configurations."""
+    DEBUG = False
+    TESTING = False
+
+
+app_config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'release': ReleaseConfig,
+}
