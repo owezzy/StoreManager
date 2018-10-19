1b6dbddc2f4989568f6ff0af4f10135ea284d465
owen_adira <owenobezzy@gmail.com>
2018-10-19 14:05:23 +0300

    [Chore #161203413] setup enviroment variables for flask instace

--- config.py
+++ config.py
@@ -3,32 +3,7 @@ This file sets the configurations for the application
 """
 import os
 
-
-class Config(object):
-    """Parent configuration class."""
-    DEBUG = False
-
-
-class DevelopmentConfig(Config):
-    """Development phase configurations"""
-    DEBUG = True
-
-
-class TestingConfig(Config):
-    """Testing Configurations."""
-    TESTING = True
-    DEBUG = True
-    DATABASE_URL = os.getenv("DATABASE_TEST_URL")
-
-
-class ReleaseConfig(Config):
-    """Release Configurations."""
-    DEBUG = False
-    TESTING = False
-
-
-app_config = {
-    'development': DevelopmentConfig,
-    'testing': TestingConfig,
-    'release': ReleaseConfig,
-}
+basedir = os.path.abspath(os.path.dirname(__file__))
+DEBUG = True
+PORT = 7000
+HOST = "127.0.0.1"
