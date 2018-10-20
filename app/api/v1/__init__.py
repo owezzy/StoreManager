b305995ba7f98693bb3113bed824539d1e01a7e9
owen_adira <owenobezzy@gmail.com>
2018-10-20 20:35:48 +0300

    [Feature #161203242] define blueprint api version1 for products resource routes

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,9 +1,13 @@
 from flask_restful import Api
-from flask import Blueprint
+from flask import Flask, Blueprint
 
-from views import ProductList, Product
+from app.api.v1.views import ProductList, Product
 
+
+app = Flask(__name__)
 version1 = Blueprint('api', __name__, url_prefix='/api/v1')
 api = Api(version1)
-api.add_resource(ProductList, '/products/')
-api.add_resource(Product, '/products/<int:id>', endpoint='product_endpoint')
+
+api.add_resource(ProductList, '/products')
+api.add_resource(Product, '/products/<int:id>')
+app.register_blueprint(version1)
