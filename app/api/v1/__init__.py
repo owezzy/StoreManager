3a7a764774a93e1f87cbdb5265b41c16f777a3ad
owen_adira <owenobezzy@gmail.com>
2018-10-20 22:15:50 +0300

    [finishes #161203357] add sales Oder resources to blueprint routes

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,7 +1,7 @@
 from flask_restful import Api
 from flask import Flask, Blueprint
 
-from app.api.v1.views import ProductList, Product
+from app.api.v1.views import ProductList, Product, OrderList,Order
 
 
 app = Flask(__name__)
@@ -10,4 +10,6 @@ api = Api(version1)
 
 api.add_resource(ProductList, '/products')
 api.add_resource(Product, '/products/<int:id>')
+api.add_resource(OrderList, '/sales')
+api.add_resource(Order, '/sales/<int:id>')
 app.register_blueprint(version1)
