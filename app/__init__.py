9841ebf375ef13eb697aac20d555e8801aadbbfb
owen_adira <owenobezzy@gmail.com>
2018-10-20 20:31:18 +0300

    [Feature #161203242] add blueprint version1 of api

--- app/__init__.py
+++ app/__init__.py
@@ -5,9 +5,9 @@ def create_app(config_filename):
     """ creates a flask instance according to config passed """
     app = Flask(__name__)
     app.config.from_object(config_filename)
-    app.url_map.strict_slashes = False
+    # app.url_map.strict_slashes = False
 
     # versions of api
-    from .api.v1 import version1 as v1
+    from app.api.v1 import version1 as v1
     app.register_blueprint(v1)
     return app
