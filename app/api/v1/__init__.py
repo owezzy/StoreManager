98080bdcfebe4c8b1c50ade9037261bf4efacab7
owen_adira <owenobezzy@gmail.com>
2018-10-20 10:37:21 +0300

    [Feature #161203242] update blueprints resource for api v1

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,9 +1,9 @@
 from flask_restful import Api
 from flask import Blueprint
 
-#from views import object
+from views import ProductList, Product
 
 version1 = Blueprint('api', __name__, url_prefix='/api/v1')
 api = Api(version1)
-
-#api.add_resource(version1, '/v1')
+api.add_resource(ProductList, '/products/')
+api.add_resource(Product, '/products/<int:id>', endpoint='product_endpoint')
