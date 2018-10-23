ab334a8983cdc406001b0dfc4702079808c64bb1
owen_adira <owenobezzy@gmail.com>
2018-10-23 16:56:44 +0300

    [feature  #161203421] update app instance config

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
+from instance.config import app_config
 
 
-def create_app(config_filename):
+def create_app(config):
     """ creates a flask instance according to config passed """
     app = Flask(__name__)
-    app.config.from_object(config_filename)
-    # app.url_map.strict_slashes = False
+    app.config.from_object(app_config[config])
 
     # versions of api
     from app.api.v1 import version1 as v1
