4a375c3a42b2e720aed0257b4fe1ec10d0744c14
Owen Adira <owenobezzy@gmail.com>
2018-10-27 13:45:51 +0300

    Revert "Bg improve code quality 161480996"

--- app/api/v1/views.py
+++ app/api/v1/views.py
@@ -1,162 +1,177 @@
-from flask import make_response, jsonify
-from flask_restful import reqparse, Resource
+from flask_restful import abort, fields, marshal_with, reqparse, Resource
+from datetime import datetime
 from app.api.v1.models import ProductsModel, SalesModel
+from pytz import timezone
 
 
-# object to define product resource
-class GetProduct(Resource):
+# timestamp set to local timezone
+def local_timezone_time_stamp():
+    tz = timezone('Africa/Nairobi')
+    ct = datetime.now(tz=tz)
+    return ct
 
+
+# product manager object
+class ProductManager:
+    last_id = 0
+
+    # using a dictionary to hold product data
+    def __init__(self):
+        self.products = {}
+
+    # adds a product to a list of products
+    def add_product(self, product):
+        self.__class__.last_id += 1
+        product.id = self.__class__.last_id
+        self.products[self.__class__.last_id] = product
+
+    def get_product(self, id):
+        return self.products[id]
+
+    def delete_product(self, id):
+        pass
+
+
+# product fields data type definition
+product_fields = {
+    'id': fields.Integer,
+    'product_name': fields.String,
+    'price': fields.Integer,
+    'stock': fields.Integer,
+    'creation_date': fields.DateTime
+}
+
+
+# product custom validation
+
+def input_validate(value):
+    if isinstance(value, int):
+        raise ValueError("The parameter cannot be a number")
+    elif value == "":
+        raise ValueError("The parameter cannot be a empty value")
+    else:
+        return value
+
+
+product_manager = ProductManager()
+
+
+# object to define products resource
+class Product(Resource):
+    # is executed in case of a request for product that doesn't exists.
     @staticmethod
-    def get():
-        all_products = ProductsModel.get_all_product()
-        return {
-                   'message': 'All Products retrieved successfully',
-                   'status': 'ok',
-                   # 'access_token': access_token,
-                   # 'refresh_token': refresh_token,
-                   'product': all_products
-               }, 200
+    def abort_if_product_doesnt_exist(id):
+        if id not in product_manager.products:
+            abort(message="Product {0} doesn't exist".format(id), http_status_code=404)
 
-
-class GetSingleProduct(Resource):
-    # retrieve a single product
-    @staticmethod
-    def get(product_id):
-        single_product = ProductsModel.get_product(product_id)
-        return {
-                   'message': 'Product retrieved successfully',
-                   'status': 'ok',
-                   # 'access_token': access_token,
-                   # 'refresh_token': refresh_token,
-                   'product_detail': single_product
-               }, 200
+    # fetch a single product
+    @marshal_with(product_fields)
+    def get(self, id):
+        self.abort_if_product_doesnt_exist(id)
+        return product_manager.get_product(id)
 
 
 # object which holds the product list
-class PostProduct(Resource):
-    @staticmethod
-    def post():
+class ProductList(Resource):
+    # fetch all products from list
+    @marshal_with(product_fields)
+    def get(self):
+        return [p for p in product_manager.products.values()]
+
+    # add a product to the list
+    @marshal_with(product_fields)
+    def post(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('product_name', type=str, required=True)
+        parser.add_argument('product_name', type=input_validate, required=True)
         parser.add_argument('price', type=int, required=True, help='can be a number only!')
         parser.add_argument('stock', type=int, required=True, help='Please specify a figure!')
 
-        # strip white space
         args = parser.parse_args()
-        product_name = args.get('product_name').strip()
-        price = args.get('price')
-        stock = args.get('stock')
-
-        # validation error
-        if not product_name:
-            return make_response(jsonify({'message': 'product_name can not be empty'}), 400)
-        if not price:
-            return make_response(jsonify({'message': 'please specify product Price!'}), 400)
-        if not stock:
-            return make_response(jsonify({'message': 'please specify product Price!'}), 400)
-
-        # check if product_name exits
-        check_product_name = ProductsModel.check_product_name(product_name)
-        if check_product_name != False:
-            return {'message': 'Product Name already Exits'}
-
-        # parse instance data to model
         product = ProductsModel(
-            product_name=product_name,
-            price=price,
-            stock=stock,
+            product_name=args['product_name'],
+            price=args['price'],
+            stock=args['stock'],
+            creation_date=local_timezone_time_stamp(),
         )
+        product_manager.add_product(product)
+        return product, 201
 
-        try:
-            result = product.add_product()
-            return {
-                       'message': 'Product was created successfully',
-                       'status': 'CREATED',
-                       # 'access_token': access_token,
-                       # 'refresh_token': refresh_token,
-                       'product_detail': result
-                   }, 201
-        except Exception as er:
-            print(er)
-        return {'message': 'Something went wrong'}, 500
+
+# sales manager object
+class SalesManager:
+    last_id = 0
+
+    def __init__(self):
+        self.orders = {}
+
+    def add_order(self, order):
+        self.__class__.last_id += 1
+        order.id = self.__class__.last_id
+        self.orders[self.__class__.last_id] = order
+
+    def get_order(self, id):
+        return self.orders[id]
+
+    def delete_order(self, id):
+        pass
+
+
+# orders payload fields data types
+order_fields = {
+    'id': fields.Integer,
+    'product_name': fields.String,
+    'attendant_name': fields.String,
+    'customer_name': fields.String,
+    'quantity': fields.Integer,
+    'cost': fields.Integer,
+    'creation_date': fields.DateTime
+}
+
+sales_manager = SalesManager()
 
 
 # Oder object to hold sales detail
-class GetOrder(Resource):
+class Order(Resource):
     @staticmethod
-    def get():
-        all_orders = SalesModel.get_all_orders()
-        return {
-                   'message': 'All Orders retrieved successfully',
-                   'status': 'ok',
-                   # 'access_token': access_token,
-                   # 'refresh_token': refresh_token,
-                   'product': all_orders
-               }, 200
+    def abort_if_order_doesnt_exist(id):
+        if id not in sales_manager.orders:
+            abort(
+                message="Order {0} doesn't exist".format(id),
+                http_status_code=404)
 
-
-class GetSingleOrder(Resource):
     # fetch a single order
-    @staticmethod
-    def get(order_id):
-        single_order = SalesModel.get_order(order_id)
-        return {
-                   'message': 'Order retrieved successfully',
-                   'status': 'ok',
-                   # 'access_token': access_token,
-                   # 'refresh_token': refresh_token,
-                   'product': single_order
-               }, 200
+    @marshal_with(order_fields)
+    def get(self, id):
+        self.abort_if_order_doesnt_exist(id)
+        return sales_manager.get_order(id)
 
 
 # OrderList object to store the sales order object
-class PostOrder(Resource):
+class OrderList(Resource):
+    # get all sales orders
+    @marshal_with(order_fields)
+    def get(self):
+        return [o for o in sales_manager.orders.values()]
+
     # add a single sales order to list
-    @staticmethod
-    def post():
+    @marshal_with(order_fields)
+    def post(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('product_name', type=str, required=True)
-        parser.add_argument('customer_name', type=str, required=True)
-        parser.add_argument('attendant_name', type=str, required=True)
+        parser.add_argument('product_name', type=input_validate, required=True)
+        parser.add_argument('customer_name', type=input_validate, required=True)
+        parser.add_argument('attendant_name', type=input_validate, required=True)
         parser.add_argument(
             'cost', type=int, required=True, help='Cost value is not valid!')
         parser.add_argument(
             'quantity', type=int, required=True, help='Please specify the quantity!')
 
         args = parser.parse_args()
-        product_name = args.get('product_name').strip()
-        customer_name = args.get('customer_name').strip()
-        attendant_name = args.get('attendant_name').strip()
-        cost = args.get('cost')
-        quantity = args.get('quantity')
-        # validation error
-        if not product_name:
-            return make_response(jsonify({'message': 'product_name can not be empty'}), 400)
-        if not customer_name:
-            return make_response(jsonify({'message': 'please specify customer name!'}), 400)
-        if not attendant_name:
-            return make_response(jsonify({'message': 'please specify attendant name!'}), 400)
-        if not cost:
-            return make_response(jsonify({'message': 'please specify cost of product!'}), 400)
-        if not quantity:
-            return make_response(jsonify({'message': 'please specify quantity!'}), 400)
-
-        new_order = SalesModel(
-            product_name=product_name,
-            customer_name=customer_name,
-            cost=cost,
-            quantity=quantity,
-            attendant_name=attendant_name
-        )
-        try:
-            result = new_order.add_order()
-            return {
-                       'message': 'Order was created successfully',
-                       'status': 'CREATED',
-                       # 'access_token': access_token,
-                       # 'refresh_token': refresh_token,
-                       'order': result
-                   }, 201
-        except Exception as er:
-            print(er)
-            return {'message': 'Something went wrong'}, 500
+        order = SalesModel(
+            product_name=args['product_name'],
+            customer_name=args['customer_name'],
+            attendant_name=args['attendant_name'],
+            cost=args['cost'],
+            quantity=args['quantity'],
+            creation_date=local_timezone_time_stamp())
+        sales_manager.add_order(order)
+        return order, 201
