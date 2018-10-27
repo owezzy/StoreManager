a62b5a8a32015c092a118e6095f889783a8bdb8f
owen_adira <owenobezzy@gmail.com>
2018-10-27 11:09:12 +0300

    [bg #161480996] update products and sales models to use better data structure

--- app/api/v1/models.py
+++ app/api/v1/models.py
@@ -1,14 +1,14 @@
 products = []
+orders = []
 
 
 class ProductsModel:
-    def __init__(self, product_name, price, stock, creation_date):
+    def __init__(self, product_name, price, stock):
         # We will automatically generate the new id
         self.id = len(products) + 1
         self.product_name = product_name
         self.price = price
         self.stock = stock
-        self.creation_date = creation_date
 
     # adds a product to a list of products
     def add_product(self):
@@ -24,8 +24,9 @@ class ProductsModel:
     def get_product(product_id):
         return next((item for item in products if item["id"] == product_id), False)
 
+    @staticmethod
     def delete_product(product_id):
-        pass
+        return products.pop(product_id)
 
     @staticmethod
     def check_product_name(product_name):
@@ -33,15 +34,29 @@ class ProductsModel:
 
 
 class SalesModel:
-    def __init__(self, product_name, attendant_name, customer_name, cost, quantity, creation_date):
+    def __init__(self, product_name, attendant_name, customer_name, cost, quantity):
         # We will automatically generate the new oder_id
-        self.id = 0
+        self.id = len(orders) + 1
         self.product_name = product_name
-        self.creation_date = creation_date
         self.attendant_name = attendant_name
         self.customer_name = customer_name
         self.quantity = quantity
         self.cost = cost
+
+    # adds a product to a list of products
+    def add_order(self):
+        new_order = {'id': self.id, 'product_name': self.product_name, 'attendant_name': self.attendant_name,
+                     'cost': self.cost, 'customer_name': self.customer_name}
+        orders.append(new_order)
+        return new_order
+
+    @staticmethod
+    def get_all_orders():
+        return orders
+
+    @staticmethod
+    def get_order(order_id):
+        return next((item for item in products if item["id"] == order_id), False)
 
 
 class UsersModel:
