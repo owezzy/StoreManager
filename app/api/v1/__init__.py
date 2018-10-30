da612225c626a698359572c18394ae839917407b
owen_adira <owenobezzy@gmail.com>
2018-10-30 06:56:48 +0300

    [Chore #161492219] add auth routes to blueprint

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,7 +1,8 @@
 from flask_restful import Api
 from flask import Flask, Blueprint
 
-from app.api.v1.views import PostProduct, GetProduct, GetSingleProduct, PostOrder, GetOrder, GetSingleOrder
+from app.api.v1.views import PostProduct, GetProduct, GetSingleProduct, PostOrder, GetOrder, GetSingleOrder, TokenRefresh
+from app.api.v1.auth import UserRegistration, UserLogin
 
 app = Flask(__name__)
 version1 = Blueprint('api', __name__, url_prefix='/api/v1')
@@ -13,4 +14,7 @@ api.add_resource(GetSingleProduct, '/products/<int:product_id>')
 api.add_resource(PostOrder, '/sales/')
 api.add_resource(GetOrder, '/sales/')
 api.add_resource(GetSingleOrder, '/sales/<int:order_id>')
+api.add_resource(UserRegistration, '/registration/')
+api.add_resource(UserLogin, '/login/')
+api.add_resource(TokenRefresh, '/token/refresh')
 app.register_blueprint(version1)
