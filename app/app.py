1fdf614028c86202f3c081fcb298785e7a5da8ca
owen_adira <owenobezzy@gmail.com>
2018-11-03 16:42:18 +0300

    [Chore #161480852] add create tables to app factory

--- app/app.py
+++ app/app.py
@@ -15,8 +15,6 @@ def create_app(config_name):
 
     # registered JWT manager
     app.config['JWT_SECRET_KEY'] = 'owezzy'
-    # app.config['JWT_BLACKLIST_ENABLED'] = True
-    # app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
     jwt = JWTManager(app)
 
     create_tables()
